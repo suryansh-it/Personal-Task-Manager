acf8a38ca83fe3974e2419a5fc3fbca0ff313af9
Suryansh Sharma <suryansharma09@gmail.com>
2024-07-17 19:50:24 +0530

    added action buttons and date

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template , redirect
-from flask import request
-from flask import url_for
+from flask import Flask, render_template , redirect , url_for,request, jsonify
+from datetime import datetime
+
 
 app = Flask(__name__)
 
-
+# In-memory storage for tasks
 tasks = []
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tasks= tasks)
+#assign value of local variable task to the var task in templates context
+#the template will have acces to var task
+
 
 
 
@@ -19,36 +22,39 @@ def create_task():
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
-        tasks.append({'title': title, 'description': description, 'done': False})  #append tasks in lists
+        current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        task = {'title': title, 'description': description, 'done': False , 'date_created': current_date}
+        tasks.append(task)  #append tasks in lists
         return redirect(url_for('index'))
     return render_template('create_task.html') #renders updated html
 
 
 
-@app.route("/edit_task/<task_id>"  , methods=['POST' , 'GET'])
+@app.route("/edit_task/<int:task_id>"  , methods=['POST' , 'GET']) #type to be specified
 def edit_task(task_id):
     task = tasks[task_id]               #task from list
     if request.method == 'POST':
         task['title'] = request.form['title']
         task['description'] = request.form['description']
-        task['done'] = 'done' in request.form
         return redirect(url_for('index'))
-    return render_template('edit_task.html')
+    return render_template('edit_task.html' , task = task , task_id =task_id)
 
-@app.route("/toggle_task/<task_id>", methods=['POST' , 'GET'])
+@app.route("/toggle_task/<int:task_id>", methods=['POST' , 'GET'])
 def toggle_task(task_id):
     task= tasks[task_id]
     if request.method == 'POST':
         task['done'] = not task['done']
         return redirect(url_for('index'))
-    return render_template('toggle_task.html')
+    return render_template('toggle_task.html' , task = task , task_id =task_id)
 
-@app.route("/delete_task/<task_id>", methods=['POST'])
+@app.route("/delete_task/<int:task_id>", methods=['POST'])
 def delete_task(task_id):
     task = tasks[task_id]
     if request.method == 'POST':
         tasks.remove(task)
-        return redirect(url_for('index'))  # Redirect to the home page after editing , client side , It changes the URL in the browser and prevents form resubmission on refresh.
+        return redirect(url_for('index'))  
+    # Redirect to the home page after editing , client side , 
+    # It changes the URL in the browser and prevents form resubmission on refresh.
 
 
 if __name__ == '__main__':
